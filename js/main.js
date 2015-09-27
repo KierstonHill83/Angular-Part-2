147f293d33a179af7c00137bd72d5aff4d741f1c
KierstonHill83 <khill1218@gmail.com>
2015-09-26 21:19:08 -0600

    Added code for reddit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,75 @@
-// add scripts
+var app = angular.module('myApp', ['angularMoment']);
 
-$(document).on('ready', function() {
-  console.log('sanity check!');
-});
+
+app.controller('redditController', ['$scope', function($scope) {
+
+  var castlewoodCanyon = {
+    'title': 'Castlewood Canyon, CO',
+    'author': 'Kierston',
+    'image': 'http://www.runningguru.com/Images/1996/castlewood%20ridge.jpg',
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    'date': Date.now(),
+    'votes': 0,
+    'comments': [{
+      'author': 'Terence',
+      'text': "This place is great!" 
+    }]
+  };
+
+  var redwood = {
+    'title': 'Redwood National Park, CA',
+    'author': 'Kierston',
+    'image': 'http://www.adventurerents.com/images/ca11180.jpg',
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    'date': Date.now(),
+    'votes': 0,
+    'comments': []
+  };
+
+  var yosemite = {
+    'title': 'Yosemite National Park, CA',
+    'author': 'Kierston',
+    'image': 'http://www.yosemitepark.com/Images/home-img-01.jpg',
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    'date': Date.now(),
+    'votes': 0,
+    'comments': []
+  };
+
+  $scope.entries = [castlewoodCanyon, redwood, yosemite];
+  $scope.showForm2 = false;
+
+
+  $scope.addEntry = function() {
+    var entry = {
+      'title': $scope.title,
+      'author': $scope.author,
+      'image': $scope.image,
+      'description': $scope.description,
+      'date': Date.now(),
+      'votes': 0,
+      'comments': []
+    };
+    $scope.entries.push(entry);
+  };
+
+
+  $scope.upvote = function() {
+    this.entry.votes++;
+  };
+
+
+  $scope.downvote = function() {
+    this.entry.votes--;
+  };
+
+
+  $scope.addComment = function() {
+    this.entry.comments.push({
+      'text': this.textComment,
+      'author': this.authorComment
+    });
+  };
+
+
+}]);
